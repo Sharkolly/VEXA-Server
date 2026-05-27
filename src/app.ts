18aabf5f8adccfc6fbab5bd9ae7894e3270c6eae
Sharkolly <sharkollymofeoluwa@gmail.com>
2026-05-27 21:21:59 +0100

    feat: new change to cors

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://client-six-liard-83.vercel.app'],
+    origin: ['http://localhost:5173', 'https://client-six-liard-83.vercel.app', 'https://vexa-shop.vercel.app/', '*'],
     credentials: true,
   }),
 );
